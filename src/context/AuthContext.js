926279109251252fd0f9f9188cd13f0cf1967b6c
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2023-02-02 19:18:34 +0200

    changes and optimisation

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,42 +1,40 @@
 import { createContext, useContext, useEffect } from "react";
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useState } from "react";
 
 const UserContext = createContext();
 
-export const AuthContextProvider = ({children}) => {
-    
-    const [user, setUser] = useState({});
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
 
-    const createUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password);
-    }
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const signIn = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password);
-    }
-    
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((currentUser) => {
-            console.log(currentUser);
-            setUser(currentUser);
-        })
-        return () => {
-            unsubscribe();
-        }
-    }, [])
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    return(
-        <UserContext.Provider value={{ createUser, signIn, user }}>
-            {children}
-        </UserContext.Provider>
-    )    
-}
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ createUser, signIn, user }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export const UserAuth = () => {
-    return useContext(UserContext);
-}
+  return useContext(UserContext);
+};
